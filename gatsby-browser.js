ec1c0ef2477ad28f23ebfcf577914de21e60283e
Jeremie Rodriguez <contact@jeremierodriguez.com>
2020-08-06 02:40:40 +0200

    feat(pwa): reload page on service worker update

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,6 +2,10 @@ const React = require('react');
 const { ThemeProvider } = require('styled-components');
 const theme = require('./src/theme').default;
 
+exports.onServiceWorkerUpdateReady = () => {
+  window.location.reload(true);
+};
+
 exports.wrapRootElement = ({ element }) => {
   return <ThemeProvider theme={theme}>{element}</ThemeProvider>;
 };
